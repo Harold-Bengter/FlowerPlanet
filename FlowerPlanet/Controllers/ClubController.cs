4dce1484fd6d0d2e6dd4a27efdf3806563c45941
Harold Bengter <Haroldbe55@yahoo.com>
2022-08-18 15:28:34 -0400

    Created Edit for club

--- FlowerPlanet/Controllers/ClubController.cs
+++ FlowerPlanet/Controllers/ClubController.cs
@@ -60,8 +60,23 @@ public class ClubController : Controller
             ModelState.AddModelError("", "Photo upload failed");
         }
 
+        return View(clubVM);    
+    }
+    public async Task<IActionResult> Edit(int id)
+    {
+        var club = await _clubRepository.GetByIdAsync(id);
+        if(club == null) return View("Error");
+        var clubVM = new EditClubViewModel
+        {
+            Title = club.Title,
+            Description = club.Description,
+            AddressId = club.AddressId,
+            Address = club.Address,
+            URL = club.Image,
+            ClubCategory = club.ClubCategory
+        };
         return View(clubVM);
-        
+
     }
 }
 
