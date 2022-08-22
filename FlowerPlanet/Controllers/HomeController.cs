e14f42686ee16c5341e858bcc9ab5a8eba552c32
Harold Bengter <Haroldbe55@yahoo.com>
2022-08-21 23:00:16 -0400

    Added home view model and ip geo locater api

--- FlowerPlanet/Controllers/HomeController.cs
+++ FlowerPlanet/Controllers/HomeController.cs
@@ -1,21 +1,54 @@
-﻿using FlowerPlanet.Models;
+﻿using FlowerPlanet.Helpers;
+using FlowerPlanet.Interfaces;
+using FlowerPlanet.Models;
+using FlowerPlanet.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Diagnostics;
+using System.Globalization;
+using System.Net;
 
 namespace FlowerPlanet.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IClubRepository _clubRepository;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IClubRepository clubRepository)
         {
             _logger = logger;
+            _clubRepository = clubRepository;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var ipInfo = new IPInfo();
+            var homeViewModel = new HomeViewModel();
+            try
+            {
+                string url = "https://ipinfo.io?token=065e05aa7a932e";
+                var info = new WebClient().DownloadString(url);
+                ipInfo = JsonConvert.DeserializeObject<IPInfo>(info);
+                RegionInfo myRI1 = new RegionInfo(ipInfo.Country);
+                ipInfo.Country = myRI1.EnglishName;
+                homeViewModel.City = ipInfo.City;
+                homeViewModel.State = ipInfo.Region;
+                if (homeViewModel.State != null)
+                {
+                    homeViewModel.Clubs = await _clubRepository.GetClubByCity(homeViewModel.City);
+                }
+                else
+                {
+                    homeViewModel.Clubs = null;
+                }
+                return View(homeViewModel);
+            }
+            catch (Exception ex)
+            {
+                homeViewModel.Clubs = null;
+            }
+            return View(homeViewModel);
         }
 
         public IActionResult Privacy()
---
+++ FlowerPlanet/ViewModels/HomeViewModel.cs
@@ -0,0 +1,10 @@
+﻿using FlowerPlanet.Models;
+
+namespace FlowerPlanet.ViewModels;
+
+public class HomeViewModel
+{
+    public IEnumerable<Club> Clubs { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+}
